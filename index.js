d227cc7624182934dc51273bd9e5a37b317fb704
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-04 09:51:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// let nu = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// // delete nu[5]
+// console.log(nu) 
+// let num = [15, 25, 35, 54, 55, 56, 75, 58, 59]
+// let numb = [156, 256, 635, 654, 655, 566, 7665, 568, 569]
+// let numbe = num.concat(nu, numb)
+// console.log(numbe,numbe.length)
+// console.log(nu,num)
+
+//sort function
+/*let comp=(a,b)=>{
+  return a-b
+}//for alingn in assending order*/
+// let comp=(a,b)=>{
+//   return b-a
+// }//for alingn in decending order
+// let mop =[98,865,5,45,4,857,458,4512,54,45,1]
+// /*mop.sort()//for align number as a string value*/
+// mop.sort(comp)
+// mop.reverse()//for reverse the numbers like accen to decen
+// console.log(mop)
+
+//splice and slice
+let pom =[98,86,5,45,4,857,458,4512,54,45,1]
+// let deltval= pom.splice(2,4,54525,695588,3669565,4556,453245)//to print the deleated value
+// console.log(deltval)
+// // console.log(pom)
+
+let number = pom.slice(4 ,6)
+// let number =num.slice(4)
+console.log(number)
